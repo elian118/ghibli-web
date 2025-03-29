cc728784534a6c8ecc505ecc492af36d3b738338
elian <elian118@naver.com>
2025-03-29 19:18:02 +0900

    refactoring

--- components/client-layer.tsx
+++ components/client-layer.tsx
@@ -3,28 +3,12 @@
 import React, { useEffect, useState } from 'react';
 import { GlobalContext, GlobalContextType, initModal, ModalState } from '@/global-context';
 import { isMobile } from '@/hooks';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-import { PaginatedFilms } from '@/generated/graphql';
+import { ApolloClient, ApolloProvider } from '@apollo/client';
+import { createApolloCache } from '@/apollo/createApolloCache';
 
 const apolloClient = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
-  cache: new InMemoryCache({
-    typePolicies: {
-      Query: {
-        fields: {
-          films: {
-            keyArgs: false,
-            merge: (existing: PaginatedFilms | undefined, incoming: PaginatedFilms): PaginatedFilms => {
-              return {
-                cursor: incoming.cursor,
-                films: existing ? [...existing.films, ...incoming.films] : incoming.films,
-              };
-            },
-          },
-        },
-      },
-    },
-  }),
+  cache: createApolloCache(),
 });
 
 const ClientLayer = ({ children }: { children: React.ReactNode }) => {
